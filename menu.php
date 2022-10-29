4e10d7eb1daee0891a04f43e17c3b907010a1c04
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-10-30 01:48:12 +0700

    Up Sistem Informasi PPDB Online Berbasis Website

--- menu.php
+++ menu.php
@@ -15,17 +15,29 @@
                 AND submenu.aktif='Y'
                 ORDER BY id_sub ASC");
                 $jml = mysql_num_rows($sub);
-            }
+            
 
             //apabila submenu ditemukan
             if ($jml > 0) {
                 echo "<ul class='sub-menu'>";
-                while ($w['top'] =='Y') {
-                    echo "<ul class='sub-menu'>";
-                    while($w ['top'] == 'Y') {
+                while ($w = myslq_fetch_array($sub)) {
+                    if ($w['top'] == 'Y'){
                         echo "<li><a href='$w[link_sub]'><span> $w[nama_sub]</span></a>";
+                        $sub2 = mysql_query("SELECT * FROM submenu WHERE id_submenu=$w[id_sub] AND id_submenu!=0");
+                        $jml2 = mysql_num_rows($sub2);
+                        if ($jml2 > 0) {
+                            echo "<ul class='sub-menu'>";
+                            while($s = myslq_fetch_array($sub2)) {
+                                echo "<li><a href='$s[link_sub]'>$s[nama_sub]</a></li>";
+                            }  echo "</ul></li>";
+                        }}else {
+                            echo "<li><a href='$w[link_sub]'> $w[nama_sub]</a>";
+                        }
+                        } echo "</li></ul>   </li>";
+                    } else { 
+                        echo "</li>";
                     }
-                }
             }
+            
     
 ?>

e2a79e2779a58e6cc57344057f6581baa0788376
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-10-30 01:19:29 +0700

    Up Sistem Informasi PPDB Online Berbasis Website

--- menu.php
+++ menu.php
@@ -8,7 +8,24 @@
                 }else {
                     echo "<li style='$r[atribut]'><a href='$[link]'>$r[nama_menu]</a>";
                 }
-                
+                $sub = mysql_query("SELECT * FROM submenu, menu 
+                WHERE submenu.id_menu = menu.id_menu 
+                AND submenu.id_menu = $r[id_menu] 
+                AND submenu.id_submenu=0
+                AND submenu.aktif='Y'
+                ORDER BY id_sub ASC");
+                $jml = mysql_num_rows($sub);
+            }
+
+            //apabila submenu ditemukan
+            if ($jml > 0) {
+                echo "<ul class='sub-menu'>";
+                while ($w['top'] =='Y') {
+                    echo "<ul class='sub-menu'>";
+                    while($w ['top'] == 'Y') {
+                        echo "<li><a href='$w[link_sub]'><span> $w[nama_sub]</span></a>";
+                    }
+                }
             }
     
 ?>
